a75c58c67a035cba39ac4fdeec545c0fd438279e
wawakaka <wawakaka72@gmail.com>
2016-04-30 17:39:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mubarak/tiket/Tab2.java
@@ -0,0 +1,22 @@
+package com.example.mubarak.tiket;
+
+/**
+ * Created by MUBARAK on 4/30/2016.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
