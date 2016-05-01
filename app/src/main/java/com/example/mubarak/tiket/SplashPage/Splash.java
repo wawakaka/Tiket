c39cda5798d44545a0ec9583cd135bd10479e9ce
wawakaka <wawakaka72@gmail.com>
2016-05-01 08:44:27 +0700

    add splash screen

---
+++ app/src/main/java/com/example/mubarak/tiket/SplashPage/Splash.java
@@ -0,0 +1,40 @@
+package com.example.mubarak.tiket.SplashPage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mubarak.tiket.MainActivity;
+import com.example.mubarak.tiket.R;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
